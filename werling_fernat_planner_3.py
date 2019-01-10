810dd0f0869522bf8c29dde53e82d08493d41f08
tzmhuang <huangtom2@gmail.com>
2019-01-10 15:07:29 +0800

    1. added cruising mode for constant speed planning 	- fixed bug in quad_poly
    2. general fixes aiming to improve code readability
    	- variable naming in classes
    	- simplified some unnecessary variable calculation and transformation.
    		e.g. time in lattice classes

--- werling_fernat_planner_3.py
+++ werling_fernat_planner_3.py
@@ -4,110 +4,124 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import math
+import threading
 
 DELTA_T = 1 # delta t in lattice
-DELTA_D = 0.5 # delta d in lateral lattice
-DELTA_S = 0.5 # delta s in longitudinal lattice
+DELTA_D = 1 # delta d in lateral lattice
+DELTA_S = 1 # delta s in longitudinal lattice
+DELTA_DS = 1 # delta ds in cruising lattice
 T_RES = 50  #sample points per second
 KJ = 0.3
 KD = 0.5
 KS = 0.5
+KDS = 2
 KT = 0.05
 D_T = 1   # Time of planning cycle (s)
-P_T = 5     # Preview Time (s)
+P_T = 3     # Preview Time (s)
 SIM_TIME = 10 # Time simulation (s)
 SIM_STEP = 0.1 #Simulation time step(s)
 ROAD_WIDTH = 2
 S_MARGIN = 5
+SPEED_LIMIT = 10
 
 
-
-
-class lat_planning():
+class frenet_trajectory_sets(): #a set of frenet trajectories within a planning cycle
     def __init__(self,t_range):
         self.t0 = t_range[0]
         self.t1 = t_range[1]
-    
+
     def gen_lat_lattice(self,d0,d_d0,dd_d0,target_path):
-        self.lat_path = target_path
         self.lat_lattice = []
         self.min_lat_cost = math.inf
-        t_horizon = np.arange(self.t0,self.t1, DELTA_T)
-
+        t_horizon = np.arange(self.t0,self.t1, DELTA_T) + DELTA_T #normalie time tstart at 0+DELTA_T
         for t in t_horizon:
-            t = t + DELTA_T #avoid t1-t0 = 0
-            t_n = t - self.t0 #normalized time
-            d_horizon = np.arange(self.lat_path.lower_eval(t),self.lat_path.upper_eval(t),DELTA_D)
-            sample_pts = np.arange(0,t_n,1/T_RES)+ self.t0
+            d_horizon = np.arange(target_path.lower_eval(t),target_path.upper_eval(t) + DELTA_D,DELTA_D)
+            sample_pts = np.arange(self.t0,t,1/T_RES)
             for d in d_horizon:
-                target_d = self.lat_path.traj.eval(t)
-                d_target_d = self.lat_path.traj.first_derivative_eval(t)
-                dd_target_d = self.lat_path.traj.second_derivative_eval(t)
-                traj = quint_poly(d0,d_d0,dd_d0,d,d_target_d,dd_target_d,self.t0,t)
+                target_d = target_path.traj.eval(t)
+                d_target_d = target_path.traj.first_derivative_eval(t)
+                dd_target_d = target_path.traj.second_derivative_eval(t)
+                traj = quint_poly(d0,d_d0,dd_d0,d,d_target_d,dd_target_d, t-self.t0)
                 traj.t_pts = sample_pts
-                traj.traj_eval = [traj.eval(p) for p in sample_pts]
+                traj.traj_eval = [traj.eval(p) for p in sample_pts-self.t0]
                 # traj.traj_d_eval = [traj.first_derivative_eval(p) for p in sample_pts]
                 # traj.traj_dd_eval = [traj.second_derivative_eval(p) for p in sample_pts]
-                traj.traj_ddd_eval = [traj.third_derivative_eval(p) for p in sample_pts]
+                traj.traj_ddd_eval = [traj.third_derivative_eval(p) for p in sample_pts-self.t0]
 
                 J = sum(np.power(traj.traj_ddd_eval,2))*1/T_RES
                 traj.cost = KJ*J + KT*t + KD*abs(d-target_d)**2
                 if self.min_lat_cost > traj.cost:
                     self.min_lat_cost = traj.cost
                     self.min_d = d
-                    self.min_t = t_n
+                    self.min_t = t
                     self.lat_opt_traj = traj
                 self.lat_lattice.append(traj)
         self.lat_opt_traj.opt = True
 
-    def plot_lat_lattice(self,ax):
-        for l in self.lat_lattice:
-            line = l.plot(ax,'grey',0.5)
-            #self.opt_traj.plot('green',1)
 
-
-
-class lon_planning():
-    def __init__(self,t_range):
-        self.t0 = t_range[0]
-        self.t1 = t_range[1]
-    
     def gen_lon_lattice(self,s0,d_s0,dd_s0,target_path): #Following/cruising
-        self.lon_path = target_path
         self.lon_lattice = []
         self.min_lon_cost = math.inf
-        t_horizon = np.arange(self.t0, self.t1, DELTA_T)
-
+        t_horizon = np.arange(self.t0, self.t1, DELTA_T) + DELTA_T# normalize time to start at 0 + DELTA_T
         for t in t_horizon:
-            t = t+DELTA_T
-            t_n = t-self.t0
-            s_horizon = np.arange(self.lon_path.lower_eval(t), self.lon_path.upper_eval(t)+0.1*DELTA_S,DELTA_S)
-            sample_pts = np.arange(0,t_n,1/T_RES) + self.t0
+            s_horizon = np.arange(target_path.lower_eval(t), target_path.upper_eval(t) + DELTA_S,DELTA_S)
+            sample_pts = np.arange(self.t0, t, 1/T_RES)
             for s in s_horizon:
-                target_s = self.lon_path.traj.eval(t)
-                d_target_s = self.lon_path.traj.first_derivative_eval(t)
-                dd_target_s = self.lon_path.traj.second_derivative_eval(t)
-                traj = quint_poly(s0,d_s0,dd_s0,s,d_target_s,dd_target_s,self.t0,t)
+                target_s = target_path.traj.eval(t)
+                d_target_s = target_path.traj.first_derivative_eval(t)
+                dd_target_s = target_path.traj.second_derivative_eval(t)
+                traj = quint_poly(s0,d_s0,dd_s0,s,d_target_s,dd_target_s, t-self.t0)
                 traj.t_pts =  sample_pts
-                traj.traj_eval = [traj.eval(p) for p in sample_pts]
+                traj.traj_eval = [traj.eval(p) for p in sample_pts-self.t0]
                 # traj.traj_d_eval = [traj.first_derivative_eval(p) for p in sample_pts]
                 # traj.traj_dd_eval = [traj.second_derivative_eval(p) for p in sample_pts]
-                traj.traj_ddd_eval = [traj.third_derivative_eval(p) for p in sample_pts]
+                traj.traj_ddd_eval = [traj.third_derivative_eval(p) for p in sample_pts-self.t0]
 
                 J  = sum(np.power(traj.traj_ddd_eval,2))*1/T_RES
                 traj.cost = KJ*J + KT*t + KS*abs(s-target_s)
                 if self.min_lon_cost > traj.cost:
                     self.min_lon_cost = traj.cost
                     self.min_s = s
-                    self.min_t = t_n
+                    self.min_t = t
                     self.lon_opt_traj = traj
                 self.lon_lattice.append(traj)
-        self.lon_opt_traj.opt = True
+            self.lon_opt_traj.opt = True
+
+
+    def cruising_lattice(self,s0,d_s0,dd_s0,target_path): #target_path = dST_path
+        self.lon_lattice = []
+        self.min_lon_cost = math.inf
+        t_horizon = np.arange(self.t0, self.t1, DELTA_T) + DELTA_T
+        for t in t_horizon:
+            ds_horizon = np.arange(target_path.lower_eval(t), target_path.upper_eval(t) + DELTA_DS, DELTA_DS)
+            sample_pts = np.arange(self.t0,t,1/T_RES)
+            for ds in ds_horizon:
+                target_ds = target_path.traj.eval(t)    #target speed
+                d_target_ds = target_path.traj.first_derivative_eval(t)     #target acceleration
+                traj = quad_poly(s0,d_s0,dd_s0,ds,d_target_ds, t - self.t0)
+                traj.t_pts = sample_pts
+                traj.traj_d_eval = [traj.first_derivative_eval(p) for p in sample_pts-self.t0]
+                traj.traj_ddd_eval = [traj.third_derivative_eval(p) for p in sample_pts-self.t0]
+
+                J = sum(np.power(traj.traj_ddd_eval,2))*1/T_RES
+                traj.cost = KJ*J + KT*t + KDS*abs(ds - target_ds)**2
+                if self.min_lon_cost > traj.cost:
+                    self.min_lon_cost = traj.cost
+                    self.min_s = ds
+                    self.min_t = t
+                    self.lon_opt_traj = traj
+                self.lon_lattice.append(traj)
+            self.lon_opt_traj.opt = True
+
+
+    def plot_lat_lattice(self,ax):
+        for l in self.lat_lattice:
+            line = l.plot(ax,'grey',0.5)
+        #self.opt_traj.plot('green',1)
 
     def plot_lon_lattice(self,ax):
         for l in self.lon_lattice:
-            l.plot(ax,'grey',0.5)
-
+            l.plot(ax,'red',0.5)
 
 
 class quint_poly():
@@ -117,14 +131,11 @@ class quint_poly():
     traj_d_eval = []
     traj_dd_eval = []
     traj_ddd_eval = []
-    def __init__(self,d0,d_d0,dd_d0,d1,d_d1,dd_d1,t0,t1):
-        self.t0 = t0
-        self.t1 = t1
+    def __init__(self,d0,d_d0,dd_d0,d1,d_d1,dd_d1,t):
         self.a0 = d0
         self.a1 = d_d0
         self.a2 = dd_d0/2
         self.end = d1
-        t = t1-t0 #normalize the t to start from 0
         #print (t)
         T = np.array([[t**3,t**4,t**5],\
                     [3*t**2,4*t**3,5*t**4],\
@@ -137,25 +148,20 @@ class quint_poly():
         self.a3, self.a4, self.a5 = np.linalg.solve(T,b)
 
     def eval(self,t):
-        t = t-self.t0
         return self.a0 + self.a1*t + self.a2*t**2 + self.a3*t**3 + self.a4*t**4 + self.a5*t**5
 
     def first_derivative_eval(self,t):
-        t = t-self.t0
         return self.a1 + 2*self.a2*t + 3*self.a3*t**2 + 4*self.a4*t**3 + 5*self.a5*t**4
 
     def second_derivative_eval(self,t):
-        t = t-self.t0
         return 2*self.a2 + 6*self.a3*t + 12*self.a4*t**2 + 20*self.a5*t**3
 
     def third_derivative_eval(self,t):
-        t = t-self.t0
         return 6*self.a3 + 24*self.a4*t + 60*self.a5*t**2
 
     def plot(self,ax,col,w):
         line = ax.plot(self.t_pts,self.traj_eval,color = col, lw = w)
         return line
-
 
 class quad_poly():
     opt = False
@@ -164,42 +170,35 @@ class quad_poly():
     traj_d_eval = []
     traj_dd_eval = []
     traj_ddd_eval = []
-    def __init__(self,d0,d_d0,dd_d0,d_d1,dd_d1,t0,t1):
-        self.t0 = t0
-        self.t1 = t1
+    def __init__(self,d0,d_d0,dd_d0,d_d1,dd_d1,t):
         self.a0 = d0
         self.a1 = d_d0
         self.a2 = dd_d0/2
-        t = t1-t0 #normalize the t to start from 0
         #print (t)
         T = np.array(
             [[3*t**2, 4*t**3],\
             [6*t, 12*t**2]]
             )
 
-        b = np.array([[d_d1-(self.a1 + self.a2*t)],\
+        b = np.array([[d_d1-(self.a1 + 2*self.a2*t)],\
                     [dd_d1 - (self.a2*2)]])
 
         self.a3, self.a4 = np.linalg.solve(T,b)
 
     def eval(self,t):
-        t = t-self.t0
         return self.a0 + self.a1*t + self.a2*t**2 + self.a3*t**3 + self.a4*t**4
 
     def first_derivative_eval(self,t):
-        t = t-self.t0
         return self.a1 + 2*self.a2*t + 3*self.a3*t**2 + 4*self.a4*t**3
 
     def second_derivative_eval(self,t):
-        t = t-self.t0
         return 2*self.a2 + 6*self.a3*t + 12*self.a4*t**2
 
     def third_derivative_eval(self,t):
-        t = t-self.t0
         return 6*self.a3 + 24*self.a4*t
 
     def plot(self,ax,col,w):
-        line = ax.plot(self.t_pts,self.traj_eval,color = col, lw = w)
+        line = ax.plot(self.t_pts,self.traj_d_eval,color = col, lw = w)
         return line
 #d0,d_d0,dd_d0,d1,d_d1,dd_d1,t_range,d_range
 # fts = frenet_trajectory_sets(3,0,0,0,0,0,[0,3],[-2,2])
@@ -242,10 +241,30 @@ class cubic_poly():
     def third_derivative_eval(self):
         return 6*self.a3
 
+class dST_path():   #speed_time path
+    def __init__(self, target, t_begin, t_end, min, max):
+        self.traj = target
+        self.begin = t_begin
+        self.end = t_end
+        self.spd_min = min
+        self.spd_max = max
 
-class ST_path():
-    def __init__(self,centre,t_begin,t_end,width):
-        self.traj = centre
+    def upper_eval(self,t):
+        return self.spd_max
+
+    def lower_eval(self, t):
+        return self.spd_min
+
+    def plot(self,ax,res):
+        pts = np.arange(self.begin,self.end,res)
+        ax.plot([x for x in pts],[self.traj.eval(y) for y in pts],color = 'blue',lw = 0.5)
+        ax.plot([x for x in pts],[self.upper_eval(y) for y in pts],color = 'blue',lw = 0.5)
+        ax.plot([x for x in pts],[self.lower_eval(y) for y in pts],color = 'blue',lw = 0.5)
+
+
+class ST_path():    #station_time path
+    def __init__(self,target,t_begin,t_end,width):
+        self.traj = target
         self.begin = t_begin
         self.end = t_end
         self.width = width
@@ -262,9 +281,9 @@ class ST_path():
         ax.plot([x for x in pts],[self.upper_eval(y) for y in pts],color = 'blue',lw = 0.5)
         ax.plot([x for x in pts],[self.lower_eval(y) for y in pts],color = 'blue',lw = 0.5)
 
-class SL_path():
-    def __init__(self,centre,t_begin,t_end,width):
-        self.traj = centre
+class SL_path():    #LatOffset_station path
+    def __init__(self,target,t_begin,t_end,width):
+        self.traj = target
         self.begin = t_begin
         self.end = t_end
         self.width = width
@@ -282,8 +301,8 @@ class SL_path():
         ax.plot([x for x in pts],[self.lower_eval(y) for y in pts],color = 'blue',lw = 0.5)
 
 class ref_path():
-    def __init__(self,centre,s_begin,s_end,width):
-        self.traj = centre                                       #'''place to insert self-drawn or spline curve'''
+    def __init__(self,target,s_begin,s_end,width):
+        self.traj = target                                       #'''place to insert self-drawn or spline curve'''
         self.begin = s_begin
         self.end = s_end
         self.path_pts()
@@ -299,9 +318,9 @@ class ref_path():
             self.cumulated_s[i] = self.ds[i-1] + self.cumulated_s[i-1]
             h = math.atan2(dy[i-1],dx[i-1]) #in radient
             self.heading[i-1] = h
-        self.heading[len(self.glb_x_pts)-1] = self.heading[len(self.glb_x_pts)-2]   #last entry always thesame as the second last entry
+        self.heading[len(self.glb_x_pts)-1] = self.heading[len(self.glb_x_pts)-2]   #last entry always the same as the second to last entry
         dh = np.diff(self.heading)
-        self.curvature = dh/self.ds #length = number_of_pts - 1
+        self.curvature = dh/self.ds                           #length = number_of_pts - 1
 
     def path_pts(self): #Always assume starting from 0,0?
         self.global_path_pts()
@@ -352,27 +371,31 @@ class simulation():
         self.reference_path = ref_path(reference_line,0,100,2*ROAD_WIDTH)            #'''reference path setting'''
         lat_line = cubic_poly(0,0,0,0) # lat_deviation - time plot              '''lateral(d) error to time path setting'''
         self.lat_target_path = SL_path(lat_line,0,SIM_TIME,ROAD_WIDTH)
-        lon_line = cubic_poly(0,10,0,0) # station - time plot                   '''longitudinal(s) error to time path setting'''
-        self.lon_target_path = ST_path(lon_line,0,SIM_TIME,S_MARGIN)
+        #lon_line = cubic_poly(5,0,0,0) # station - time plot                   '''longitudinal(s) error to time path setting'''
+        #self.lon_target_path = ST_path(lon_line,0,SIM_TIME,DS_MARGIN)
+        cruise_dST_plt = cubic_poly(8,0,0,0)
+        self.lon_target_path = dST_path(target = cruise_dST_plt, t_begin = 0, t_end = SIM_TIME, min = 0, max = SPEED_LIMIT)
 
     def next(self):
         self.t = self.t + SIM_STEP
 
     def update_pos(self):
-        self.d0 = self.lat_fts.lat_opt_traj.eval(self.t)[0]
-        self.d_d0 = self.lat_fts.lat_opt_traj.first_derivative_eval(self.t)[0]
-        self.dd_d0 = self.lat_fts.lat_opt_traj.second_derivative_eval(self.t)[0]
-        self.s0 = self.lon_fts.lon_opt_traj.eval(self.t)[0]
-        self.d_s0 = self.lon_fts.lon_opt_traj.first_derivative_eval(self.t)[0]
-        self.dd_s0 = self.lon_fts.lon_opt_traj.second_derivative_eval(self.t)[0]
+        self.d0 = self.fts.lat_opt_traj.eval(self.t-self.planing_start_t)[0]
+        self.d_d0 = self.fts.lat_opt_traj.first_derivative_eval(self.t-self.planing_start_t)[0]
+        self.dd_d0 = self.fts.lat_opt_traj.second_derivative_eval(self.t-self.planing_start_t)[0]
+        self.s0 = self.fts.lon_opt_traj.eval(self.t-self.planing_start_t)[0]
+        self.d_s0 = self.fts.lon_opt_traj.first_derivative_eval(self.t-self.planing_start_t)[0]
+        self.dd_s0 = self.fts.lon_opt_traj.second_derivative_eval(self.t-self.planing_start_t)[0]
 
     def plan(self):
-        self.lat_fts = lat_planning([self.t, self.t + P_T])
-        self.lon_fts = lon_planning([self.t, self.t + P_T])
-        self.lat_fts.gen_lat_lattice(self.d0,self.d_d0,self.dd_d0,self.lat_target_path)
-        self.lat_fts.plot_lat_lattice(self.ax2[1])
-        self.lon_fts.gen_lon_lattice(self.s0,self.d_s0,self.dd_s0,self.lon_target_path)
-        self.lon_fts.plot_lon_lattice(self.ax2[0])
+        self.planing_start_t = self.t
+        self.fts = frenet_trajectory_sets([self.planing_start_t,self.planing_start_t + P_T])
+        self.fts.gen_lat_lattice(self.d0,self.d_d0,self.dd_d0,self.lat_target_path)
+        self.fts.plot_lat_lattice(self.ax2[1])
+        # self.fts.gen_lon_lattice(self.s0,self.d_s0,self.dd_s0,self.lon_target_path)
+        self.fts.cruising_lattice(self.s0,self.d_s0,self.dd_s0,self.lon_target_path)
+        self.fts.plot_lon_lattice(self.ax2[0])
+        print('*************time:',self.t, self.fts.lon_opt_traj.traj_d_eval,'\n opt_traj:', self.fts.lon_opt_traj.a2)
 
 
     def init_main_plot(self):
@@ -396,8 +419,8 @@ class simulation():
         self.ax2[1].grid(linewidth = 1, ls = '--')
         self.ax2[1].set_ylim(-5,12)
         self.ax2[1].set_xlim(0,SIM_TIME)
-        self.line_lon, = self.ax2[0].plot([],[],color = 'green')
-        self.line_lat, = self.ax2[1].plot([],[],color = 'green')
+        self.line_lon, = self.ax2[0].plot([],[],color = 'green',zorder = math.inf)
+        self.line_lat, = self.ax2[1].plot([],[],color = 'green',zorder = math.inf)
         self.lon_xdata,self.lon_ydata = [],[]
         self.lat_xdata,self.lat_ydata = [],[]
         self.line_lon.set_data(self.lon_xdata,self.lon_ydata)
@@ -406,8 +429,8 @@ class simulation():
     def refresh_plots(self):
         self.lon_xdata.append(self.t)
         self.lat_xdata.append(self.t)
-        self.lon_ydata.append(self.lon_fts.lon_opt_traj.eval(self.t)[0])
-        self.lat_ydata.append(self.lat_fts.lat_opt_traj.eval(self.t)[0])
+        self.lon_ydata.append(self.d_s0)
+        self.lat_ydata.append(self.d0)
         self.line_lon.set_data(self.lon_xdata,self.lon_ydata)
         self.line_lat.set_data(self.lat_xdata,self.lat_ydata)
         self.rect.set_visible(False)
@@ -423,7 +446,7 @@ class simulation():
         self.ax.add_patch(self.rect)
 
     def log(self):
-        print ('start:[ t = {}, d0 = {}, d_d0 = {}, dd_d0 = {} ] COST = {}'.format(self.t,self.d0, self.d_d0, self.dd_d0,self.lat_fts.lat_opt_traj.cost))
+        print ('start:[ t = {}, d0 = {}, d_d0 = {}, dd_d0 = {} ] COST = {}'.format(self.t,self.d0, self.d_d0, self.dd_d0,self.fts.lat_opt_traj.cost))
 
 def spin():
     sim = simulation()
@@ -432,8 +455,8 @@ def spin():
     cycle_count = 0
     while sim.t <= SIM_TIME:
         if cycle_count%int(D_T/SIM_STEP) == 0:
-            print ("PLANNED")
             sim.plan()
+            print ("PLANNED")
         sim.refresh_plots()
         sim.next()
         sim.update_pos()
